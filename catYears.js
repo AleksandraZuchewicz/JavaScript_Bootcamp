458ce880e817fb20961038a320b0cc16a64fe8d1
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-04-04 18:52:01 +0100

    Cat years converter

---
+++ catYears.js
@@ -0,0 +1,18 @@
+let myAge = 25;
+
+let earlyYears = 2;
+earlyYears *= 25;
+var laterYears = myAge - 2;
+laterYears *= 4;
+
+myAgeInCatYears = earlyYears + laterYears;
+let myName = "Aleksandra";
+console.log(
+  "My nane is " +
+    myName +
+    " I am " +
+    myAge +
+    " years old in human  which is " +
+    myAgeInCatYears +
+    " yers old in cat years."
+);
