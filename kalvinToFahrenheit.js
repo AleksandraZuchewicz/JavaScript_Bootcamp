65d804105c6710e2788786c97f4a69c3cfbb1db5
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-04-04 18:31:35 +0100

    Kalvin to Fahrenheit calculator

---
+++ kalvinToFahrenheit.js
@@ -0,0 +1,5 @@
+let kelvinTemp = 301;
+let celsiusTemp = kelvinTemp - 273.15;
+let fahrenheitTemp = Math.floor(celsiusTemp * (9 / 5) + 32);
+
+console.log("The temperature is " + fahrenheitTemp + " degrees Fahrenheit");
