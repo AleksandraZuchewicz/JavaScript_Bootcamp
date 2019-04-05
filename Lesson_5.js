d07e74dcbc61e8420e93b176ce5de51149f08e6c
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-04-05 12:04:14 +0100

    Arrays

---
+++ Lesson_5.js
@@ -0,0 +1,20 @@
+//Arrays
+
+let testScores = [72, 84, 68, 92, 74];
+let homeRuns = [4, 67, 8, 5, 6, 8];
+let cookie = ["sugar", "butter", "flower"];
+
+//Accesed by index array[index]
+
+homeRuns[2];
+testScores[0];
+
+//untype elements in an array
+let fishTank = ["glass", 10, "gols fish", true];
+
+let baseballTeam = ["Legia", "Plock", "Wks", "Rybnik", "Slask"];
+
+baseballTeam[5] = "Warszawa";
+baseballTeam[baseballTeam.length] = "wol";
+
+console.log(baseballTeam);
